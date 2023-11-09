1176b37919cd7545ce6ee0dddb42e7eed7253875
Nahom <derebeam2123@gmail.com>
2023-11-09 01:40:17 +0000

    task all

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,70 +1,96 @@
 #!/usr/bin/env python3
-""" Module Basic_auth for user authentication
 """
-from typing import TypeVar, Tuple
-from base64 import b64decode, decode
-from api.v1.auth.auth import Auth
-from models.user import User
+Definition of class BasicAuth
+"""
 import base64
+from .auth import Auth
+from typing import TypeVar
+
+from models.user import User
 
 
 class BasicAuth(Auth):
-    """ Extends BasicAuth class
+    """ Implement Basic Authorization protocol methods
     """
-    def extract_base64_authorization_header(self, auth_header: str) -> str:
-        """ Extract base64 authorization header """
-        if auth_header is None or not isinstance(auth_header, str):
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for a Basic
+        Authorization
+        """
+        if authorization_header is None:
             return None
-        if 'Basic ' not in auth_header:
+        if not isinstance(authorization_header, str):
             return None
-        return auth_header[6:]
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
 
-    def decode_base64_authorization_header(self, b64_auth_header: str) -> str:
-        """ Returns decode base64 authorization """
-        if b64_auth_header is None or not isinstance(b64_auth_header, str):
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Decode a Base64-encoded string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
             return None
         try:
-            b64 = base64.b64decode(b64_auth_header)
-            b64_decode = b64.decode('utf-8')
+            decoded = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(decoded)
+            return decoded.decode('utf-8')
         except Exception:
             return None
-        return b64_decode
 
-    def extract_user_credentials(
-            self, decoded_b64_auth_header: str) -> (str, str):
-        """ Returns user credentials """
-        if decoded_b64_auth_header is None or not isinstance(
-                decoded_b64_auth_header, str) \
-           or ':' not in decoded_b64_auth_header:
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        Returns user email and password from Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
             return (None, None)
-        return decoded_b64_auth_header.split(':', 1)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        email = decoded_base64_authorization_header.split(":")[0]
+        password = decoded_base64_authorization_header[len(email) + 1:]
+        return (email, password)
 
-    def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """ Returns user object from credentials """
-        if user_email is None or not isinstance(
-                user_email, str) or user_pwd is None or not isinstance(
-                    user_pwd, str):
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Return a User instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
         try:
-            users = User.search({'email': user_email})
-        except Exception:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
             return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+        except Exception:
             return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Overloads Basic authentication """
-        auth_header = self.authorization_header(request)
-        if not auth_header:
-            return None
-        extract_base64 = self.extract_base64_authorization_header(auth_header)
-        decode_base64 = self.decode_base64_authorization_header(extract_base64)
-        user_credentials = self.extract_user_credentials(decode_base64)
-        user_email = user_credentials[0]
-        user_password = user_credentials[1]
-        user_credentials = self.user_object_from_credentials(
-            user_email, user_password)
-        return user_credentials
+        """
+        Returns a User instance based on a received request
+        """
+        Auth_header = self.authorization_header(request)
+        if Auth_header is not None:
+            token = self.extract_base64_authorization_header(Auth_header)
+            if token is not None:
+                decoded = self.decode_base64_authorization_header(token)
+                if decoded is not None:
+                    email, pword = self.extract_user_credentials(decoded)
+                    if email is not None:
+                        return self.user_object_from_credentials(email, pword)
+        return
